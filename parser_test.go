96a50afd013208708cdf612acfb0561823d28d33
wardonet2020 <1730314864@qq.com>
2025-09-16 10:07:32 +0800

    test(parser): 新增测试代码以验证 APK 解析功能

--- parser_test.go
+++ parser_test.go
@@ -26,6 +26,54 @@ func TestAppParser(t *testing.T) {
 	t.Logf("TargetSdkVersion: %v\n", app.TargetSdkVersion)
 	t.Logf("MaxSdkVersion: %v\n", app.MaxSdkVersion)
 	t.Logf("Md5: %v\n", app.Md5)
+	t.Logf("Sha1: %v\n", app.Sha1)
+	t.Logf("Sha256: %v\n", app.Sha256)
+	t.Logf("Signature md5: %v\n", app.CertInfo.Md5)
+
+	if app.Icon != nil {
+		// 生成png的icon
+		pngFile, err := os.Create("testdata/helloworld.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			_ = pngFile.Close()
+		}()
+		// 将 img 保存为 PNG 格式的图片文件
+		err = png.Encode(pngFile, app.Icon)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func TestAppParser_NewFromReader(t *testing.T) {
+	apkFile := "testdata/helloworld.apk"
+	f, err := os.Open(apkFile)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer f.Close()
+	app, err := NewFromReader(f, Option{
+		WithIcon:             true,
+		WithSignature:        true,
+		IgnoreSignatureError: true,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("Name: %v\n", app.Name)
+	t.Logf("BundleId: %v\n", app.BundleId)
+	t.Logf("Version: %v\n", app.Version)
+	t.Logf("Build: %v\n", app.Build)
+	t.Logf("MinSdkVersion: %v\n", app.MinSdkVersion)
+	t.Logf("TargetSdkVersion: %v\n", app.TargetSdkVersion)
+	t.Logf("MaxSdkVersion: %v\n", app.MaxSdkVersion)
+	t.Logf("Md5: %v\n", app.Md5)
+	t.Logf("Sha1: %v\n", app.Sha1)
+	t.Logf("Sha256: %v\n", app.Sha256)
 	t.Logf("Signature md5: %v\n", app.CertInfo.Md5)
 
 	if app.Icon != nil {
